f04baaa1a71885a98bc97dee5f4499636b5dba62
Parker Lange <parker03@ksu.edu>
2019-11-11 17:11:14 -0600

    Class work 11/11

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+
+document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', onClick);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerHTML = currentTurn; 
+        if (!checkForWin()) nextTurn(); 
+    }
+    
+}
+
+function nextTurn() {
+    if (currentTurn === "X") {
+        currentTurn = "O";
+    }
+    else {
+        currentTurn = "X";
+    }
+
+    document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+}
+
+function declareWinner() {
+    document.getElementById("turn").innerText = "It is player " + currentTurn + " wins!";
+    currentTurn = "";
+}
+
+function checkForWin() {
+    // check rows
+    for (var i = 0; i < 9; i += 3) {
+        if (squares[i].innerText && squares[i + 0].innerText === squares.[i + 1].innerText && squares[i + 1].innerText === squares[i + 2].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+
+    // check columns
+    for (var j = 0; j < 3; j += 1) {
+        if (squares[i].innerText && squares[j].innerText === squares.[j + 3].innerText && squares[j + 3].innerText === squares[j + 6].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+
+    // check diagonals 
+
+    return false;
+}
