799d766fd915b644f7849e5b80d3f8b0d5c2eda6
Parker Lange <parker03@ksu.edu>
2019-11-15 17:04:34 -0600

    class work 11/15

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -58,3 +58,33 @@ function checkForWin() {
 
     return false;
 }
+
+var squares = document.querySelectorAll("square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('dragenter', onDragEnter);
+    squares[i].addEventListener('dragleave', onDragLeave);
+    squares[i].addEventListener('dragleave', onDragStart);
+    squares[i].addEventListener('dragleave', onDragEnd);
+}
+
+function onDragEnter(event) {
+    if (event.target.classList.contain("checker")) return;
+    if (event.target.classList.contain("red")) return;
+    if (event.target.children.length > 0) return; 
+    event.target.style.backgroundColor = "gold";
+    document.getElementById("toX").value = event.target.dataset.x;
+    document.getElementById("toY").value = event.target.dataset.y;
+}
+
+function onDragLeave(event) {
+    event.target.style.backgroundColor = null;
+}
+
+function onDragStart(event) {
+    document.getElementById("fromX").value = event.target.dataset.x;
+    document.getElementById("fromY").value = event.target.dataset.y; 
+}
+
+function onDragEnd(event) {
+    document.getElementsByName("checkers-form").submit(); 
+}
